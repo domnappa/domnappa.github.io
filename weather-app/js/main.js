d568fd5c5482d5171f507148532a9bd5a605159a
Dom <dom@steadlane.com.au>
2021-11-23 22:43:41 +1000

    added a lot of new styling added input support added country conversion for display added support for country search

--- weather-app/js/main.js
+++ weather-app/js/main.js
@@ -1,9 +1,25 @@
+let notifier = new AWN({
+  "position": "bottom-right"
+});
+
 $(document).ready(function() {
   let api_key = "9dad23fcdbe30ae17ead09ad9fa3cc40";
-  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+  let shorthand_days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  let longhand_days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+
   let today = new Date();
-  //let request_url = `https://api.openweathermap.org/data/2.5/weather?q=Brisbane,au&APPID=${api_key}`;
   let geolocation_enabled = 'geolocation' in navigator;
+
+  function findNth(d) {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
 
   function generateFutureForecast(position) {
     let request_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${api_key}&units=metric&exclude=current,hourly,minutely`;
@@ -13,7 +29,7 @@ $(document).ready(function() {
 
       for(let i=1;i<6;i++) {
         let daily_data = data.daily[i];
-        let name_of_day = days[today.getDay()+i];
+        let name_of_day = shorthand_days[today.getDay()+i];
         console.log(daily_data);
         let html_output = `
         <div class="day-container">
@@ -32,33 +48,71 @@ $(document).ready(function() {
 
         $forecast_container.append(html_output);
       }
+
+      $forecast_container.addClass('shown');
     });
   }
 
   function initGeolocationData(position) {
     let request_url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${api_key}&units=metric`;
     $.get(request_url, function(data) {
-      let $today_container = $('.today');
-
-      let html_output = `
-      <div class="day">${days[today.getDay()]}</div>
-      <div class="cloud-coverage">${data.clouds.all}%</div>
-      <div class="temperature">
-        <div class="current-temp">${Math.round(data.main.temp)}°</div>
-        <div class="min-temp">${Math.round(data.main.temp_min)}°</div>
-        <div class="max-temp">${Math.round(data.main.temp_max)}°</div>
-        <div class="humitidy">${data.main.humidity}%</div>
-      </div>`;
-
-        $today_container.append(html_output);
-
-        generateFutureForecast(position);
+      parseTodaysData(data);
+      generateFutureForecast(position);
     });
 
+  }
+
+  function parseTodaysData(data) {
+    console.log(data);
+    let $today_container = $('.today');
+    $today_container.empty();
+    $('.no-content').removeClass('shown');
+    let date_arr = today.toDateString().split(' ');
+
+    let html_output = `
+    <div class="day" style="display:none;">${longhand_days[today.getDay()] + ', ' + date_arr[1] + ' ' + date_arr[2] + findNth(today.getDate())}</div>
+    <div class="location">${data.name}, ${regionNames.of(data.sys.country)}</div>
+    <div class="cloud-coverage">Cloud Coverage: ${data.clouds.all}%</div>
+    <div class="humitidy">Humidity: ${data.main.humidity}%</div>
+    <div class="temperature">
+      <div class="min-temp">${Math.round(data.main.temp_min)}°</div>
+      <div class="current-temp">
+        <div class="weather-icon"><img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" /></div>
+        <span class="temp">${Math.round(data.main.temp)}°</span>
+      </div>
+      <div class="max-temp">${Math.round(data.main.temp_max)}°</div>
+    </div>
+    <div class="weather-meta text-center">${data.weather[0].description}</div>`;
+
+      $today_container.append(html_output);
+  }
+
+  function initInputLocationDate(location) {
+    let city, country, request_url;
+    if(location.includes(",")) {
+      city = location.split(",")[0];
+      country = location.split(",")[1];
+      request_url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}&units=metric`;
+    } else {
+      request_url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&units=metric`;
+    }
+
+    $.get(request_url, function(data) {
+      parseTodaysData(data);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      notifier.alert(jqXHR.responseJSON.message);
+    });
   }
 
   if(geolocation_enabled) {
     navigator.geolocation.getCurrentPosition(initGeolocationData)
   }
+
+  $(".location-input input").on('keyup', function (e) {
+      if (e.key === 'Enter' || e.keyCode === 13) {
+        initInputLocationDate($(this).val());
+      }
+  });
 
 })
